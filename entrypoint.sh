11d0415b2a45226d05be33b3253594186a7f3511
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-04-24 21:42:00 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 rubocop \
   --require ./rdjson_formatter/rdjson_formatter.rb \
   --format RdjsonFormatter \
-  --fail-level error  | reviewdog -f=rdjson -reporter=github-pr-review -level=warning -guess
+  --fail-level error test/only_changed/few_relevant/files/ | reviewdog -f=rdjson -reporter=github-pr-review -level=warning -guess
