cbfa49402beffe3220a0d2b75376f8b8b9ea74aa
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-04-24 09:28:42 +0900

    rm reviewdog

--- script.sh
+++ script.sh
@@ -127,15 +127,7 @@ ${BUNDLE_EXEC}rubocop \
   --format RdjsonFormatter \
   --fail-level error \
   ${INPUT_RUBOCOP_FLAGS} \
-  "${CHANGED_FILES[@]}" \
-  | reviewdog -f=rdjson \
-      -name="${INPUT_TOOL_NAME}" \
-      -reporter="${INPUT_REPORTER}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-level="${INPUT_FAIL_LEVEL}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+  "${CHANGED_FILES[@]}"
 
 reviewdog_rc=$?
 echo '::endgroup::'
